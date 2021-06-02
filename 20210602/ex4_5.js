c586cca2776c5104037b60e8b57368d64011ddf2
hieuptvmo <85218158+hieuptvmo@users.noreply.github.com>
2021-06-02 17:20:32 +0700

    Add files via upload

--- 20210602/ex4_5.js
+++ 20210602/ex4_5.js
@@ -1,10 +1,10 @@
-var number = [4, 2, 5, 1, 2, 3, 5, 9, 8, 1];
+var numbers = [4, 2, 5, 1, 2, 3, 5, 9, 8, 1];
 
 //// Sử dụng reduce để loại bỏ những số trùng lặp, vd: có 2 số 1 ở trong mảng trên thì loại bỏ 1 số, chỉ giữ lại 1 số 1
 
 let remove = [];
 
-remove = number.reduce((arr, value) => {
+remove = numbers.reduce((arr, value) => {
     if (arr.indexOf(value) === -1) {
         arr.push(value);
     }
@@ -15,8 +15,10 @@ console.log(remove)
 
 //// Sử dụng reduce để tính tổng tất cả các số trong array
 
-let sum = number.reduce((a, b) => {
-    return a + b;
-});
+// let sum = numbers.reduce((a, b) => {
+//     return a + b;
+// });
+
+let sum = numbers.reduce((a, b) => a + b, 0)
 
 console.log(sum)
