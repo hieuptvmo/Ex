11ab975c92ee7309fd85c532cc537f5a47c80196
hieuptvmo <85218158+hieuptvmo@users.noreply.github.com>
2021-06-02 14:30:54 +0700

    Add files via upload

--- 20210602/ex6.js
+++ 20210602/ex6.js
@@ -1,9 +1,14 @@
-var number = [4, 2, 5, 1, 2, 3, 5, 9, 8, 1];
-
 //// Viết 1 function getTotalObjectValue 1 parameter truyền vào obj và 1 function getTotalArrayValue 2 paramter, 1 la arr, 
 //// 2 la callback, function getTotalObjectValue cần kiểm tra obj truyền vào có valid không, nếu không sẽ trả về lỗi, 
 //// nếu valid thì lấy hết value của obj đó chuyển thành 1 array, sau đó gọi đến getTotalArrayValue để tính tổng các số 
 //// trong array đó và dùng callback để lấy dữ liệu về
+
+function objectCreate(num1, num2, num3, num4, num5) {
+    let obj = {num1, num2, num3, num4, num5};
+    return obj;
+}
+
+const object = new objectCreate(4, 5, 1, 3, 2);
 
 function getTotalObjectValue(obj) {
     if (Object.keys(obj) == 0) {
@@ -12,7 +17,7 @@ function getTotalObjectValue(obj) {
     return Object.values(obj);
 }
 
-let result = getTotalObjectValue(number);
+let result = getTotalObjectValue(object);
 
 function getTotalArrayValue(arr, cb) {
     if(arr == false) {
